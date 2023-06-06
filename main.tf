ee4a91a835c8208dbf03642c817bddf7374ab3f7
kevin-loehfelm <kevin.loehfelm@hashicorp.com>
2023-06-05 22:55:51 -0400

    Add region output

--- main.tf
+++ main.tf
@@ -1,17 +1,20 @@
 module "prod1" {
   source  = "app.terraform.io/kloehfelm-demo/network-aws/aws"
-  version = "0.0.1"
+  version = "0.0.4"
 
-  label      = "use1-prod"
-  aws_region = "us-east-1"
+  label                   = "use1-prod"
+  aws_region              = "us-east-1"
+  enable_nat              = true
+  enable_database_subnets = true
   tags = {
     "env:site" : "production"
   }
 }
 
+/*
 module "dr" {
   source  = "app.terraform.io/kloehfelm-demo/network-aws/aws"
-  version = "0.0.1"
+  version = "0.0.4"
 
   label      = "usw2-dr"
   aws_region = "us-west-2"
@@ -19,3 +22,4 @@ module "dr" {
     "env:site" : "dr"
   }
 }
+*/
--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,14 @@ output "prod1" {
   value       = module.prod1.vpc
 }
 
+output "prod1_region" {
+  description = "aws production 1 region"
+  value = var.aws_region
+}
+
+/*
 output "dr" {
   description = "aws dr 1 vpc"
   value       = module.dr.vpc
 }
+*/
